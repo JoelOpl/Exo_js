601c778dc1b18b332d838840a8abebc6451f1dad
Joelopl <omplou.joel@yahoo.fr>
2020-09-23 12:10:47 +0200

    chore:me exocise js

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * Exercice 1
+ */
+/*Exercice 1*/
+
+// My digital School Forever
+
+/*Exercice 3*/
+var String = "42";
+var Nomber = 42;
+var Float = 42.0;
+var Boolean = true;
+var Array = [42];
+var Date = 42;
+var Objet = { Nomber1: "42" };
+var Underfined;
+var Null = null;
+console.log(String, Nomber, Float, Boolean, Array, Date, Objet, Null);
+/*Exercice 4*/
+var my42count = "quarante-deux";
+/*Exercice 5*/
+console.log(my42count.length);
+/*Exercice 6*/
+var myArray42 = ["q", "u", "a", "r", "a", "n", "t", "e", "d", "e", "u", "x"];
